354cce33f3e91fc8a7b130710953995e227780d1
fastsofts <fastsoftsolutions@gmail.com>
2018-04-17 21:51:47 +0530

    iphone fix
    iphone fix

--- js/uganda.map.js
+++ js/uganda.map.js
@@ -1711,7 +1711,7 @@ function detectmob() {
         $("#right").find(".toggler").trigger("click");
        } 
     setTimeout(function(){ 
-       if ($(window).width() < 600 && detectmob()))
+       if ($(window).width() < 600 && detectmob())
           { 
            $("#left").find(".toggler").trigger("click");
           }
@@ -1740,7 +1740,7 @@ function detectmob() {
       idclicked = $(this).parent().attr("id");
       e.stopPropagation();
       e.preventDefault(); 
-      if ($(window).width() < 600 && detectmob()))
+      if ($(window).width() < 600 && detectmob())
          {     
           tclick = true;    
           setTimeout(function(){   
